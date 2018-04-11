9687117fb248a648a9f7d8340911105b29380ded
Douglas Bebber <dab@i2pmail.org>
2018-04-10 22:09:27 -0500

    Create crypto-lock-decrypt.sh

---
+++ demos/file-encryption/crypto-lock-decrypt.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# generate a random string for the challenge.
+function randomString {
+        # if a param was passed, it's the length of the string we want
+        if [[ -n $1 ]] && [[ "$1" -lt 20 ]]; then
+                local myStrLength=$1;
+        else
+                # otherwise set to default
+                local myStrLength=8;
+        fi
+
+        local mySeedNumber=$$`date +%N`; # seed will be the pid + nanoseconds
+        local myRandomString=$( echo $mySeedNumber | md5sum | md5sum );
+        # create our actual random string
+        myRandomResult="${myRandomString:2:myStrLength}"
+}
+
+randomString 10;
+echo "Challenge: $myRandomResult";
+read filename challenge address signature
+
+buff=`curl -X POST http://127.0.0.1:18082/json_rpc -d '{"jsonrpc":"2.0","id":"0","method":"verify","params":{"data":"'"$challenge"'","address":"'"$address"'","signature":"'"$signature"'"}}' -H 'Content-Type: application/json'`
+
+pass="true"
+if echo "$buff" | grep -q "$pass"; then
+ fname=$(basename -- "$filename")
+ extension="$fname##*.}"
+ fname="${fname%.*}"
+ path=$(dirname "${filename}")
+ gpg --passphrase "$address" --output "$path"/"$fname"  "$filename" ;
+else
+   echo "Request is NOT authenticated!";
+fi
