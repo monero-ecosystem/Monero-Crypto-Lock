03f40ebb2afd1285f2a0b8abd1cbe58150eb050a
Douglas Bebber <dab@i2pmail.org>
2018-04-11 23:39:56 -0500

    Update crypto-lock-sign.sh

--- demos/basic/crypto-lock-sign.sh
+++ demos/basic/crypto-lock-sign.sh
@@ -8,7 +8,7 @@ echo "Challenge: $1"
 echo "####################################"
 
  # Echo the Monero wallet address:
-curl -i -X POST -H "Content-Type: application/json" -d '{"jsonrpc":"2.0","id":"0","method":"getaddress"}' http://127.0.0.1:18082/json_rpc 
+curl -i -X POST -H "Content-Type: application/json" -d '{"jsonrpc":"2.0","id":"0","method":"getaddress"}' http://127.0.0.1:18083/json_rpc 
 
 # Sign the challenge string and echo the signature:
-curl -i -X POST http://127.0.0.1:18082/json_rpc -d '{"jsonrpc":"2.0","id":"0","method":"sign","params":{"data":"'"$challenge"'"}}' -H 'Content-Type: application/json'
+curl -i -X POST http://127.0.0.1:18083/json_rpc -d '{"jsonrpc":"2.0","id":"0","method":"sign","params":{"data":"'"$challenge"'"}}' -H 'Content-Type: application/json'
