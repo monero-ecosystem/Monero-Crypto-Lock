82649c9d563355e890a73241bedf79ba2c8c035b
Douglas Bebber <dab@i2pmail.org>
2018-04-11 23:41:28 -0500

    Update crypto-lock-decrypt.sh

--- demos/file-encryption/crypto-lock-decrypt.sh
+++ demos/file-encryption/crypto-lock-decrypt.sh
@@ -20,7 +20,7 @@ randomString 10;
 echo "Challenge: $myRandomResult";
 read filename challenge address signature
 
-buff=`curl -X POST http://127.0.0.1:18082/json_rpc -d '{"jsonrpc":"2.0","id":"0","method":"verify","params":{"data":"'"$challenge"'","address":"'"$address"'","signature":"'"$signature"'"}}' -H 'Content-Type: application/json'`
+buff=`curl -X POST http://127.0.0.1:18083/json_rpc -d '{"jsonrpc":"2.0","id":"0","method":"verify","params":{"data":"'"$challenge"'","address":"'"$address"'","signature":"'"$signature"'"}}' -H 'Content-Type: application/json'`
 
 pass="true"
 if echo "$buff" | grep -q "$pass"; then
