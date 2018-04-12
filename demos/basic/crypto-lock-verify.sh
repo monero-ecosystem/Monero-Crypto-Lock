a4558b272a47c5f690c36c9626651285d97156d4
Douglas Bebber <dab@i2pmail.org>
2018-04-11 23:40:52 -0500

    Update crypto-lock-verify.sh

--- demos/basic/crypto-lock-verify.sh
+++ demos/basic/crypto-lock-verify.sh
@@ -13,5 +13,5 @@ echo "Signature: $3"
 echo "################################"
 
 # now verify that the signature was valid for the Monero public address and the challenge string.
-curl -X POST http://127.0.0.1:18082/json_rpc -d '{"jsonrpc":"2.0","id":"0","method":"verify","params":{"data":"'"$1"'","address":"'"$2"'","signature":"'"$3"'"}}' -H 'Content-Type: application/json'
+curl -X POST http://127.0.0.1:18083/json_rpc -d '{"jsonrpc":"2.0","id":"0","method":"verify","params":{"data":"'"$1"'","address":"'"$2"'","signature":"'"$3"'"}}' -H 'Content-Type: application/json'
 
