c10ef6f6fa745436ce78a29639757ded248fec29
Douglas Bebber <dab@i2pmail.org>
2018-04-18 10:57:09 -0500

    Add files via upload
    added command line argument for lock-box and general cleanup of code.

--- demos/lock-box/unlock-lock-box.sh
+++ demos/lock-box/unlock-lock-box.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
-# generate a random string for the challenge.
+if [ -z "$1" ]
+   then
+        echo "must specify full path specifier for lock-box!"
+        exit 1
+fi
+lock_box=$1
+
 function randomString {
         # if a param was passed, it's the length of the string we want
         if [[ -n $1 ]] && [[ "$1" -lt 20 ]]; then
@@ -18,7 +24,7 @@ function randomString {
 
 randomString 10;
 echo "Challenge: $myRandomResult";
-# Get the challenge string, address, and signature from the console. each separated by a space.
+
 read challenge address signature
 
 #echo "$challenge $address $signature"
@@ -33,20 +39,18 @@ echo "################################"
 
 
 #curl -i -X POST -H "Content-Type: application/json" -d '{"jsonrpc":"2.0","id":"0","method":"getaddress"}' http://127.0.0.1:18082/json_rpc
-# verify the digital signature for the challenge string by the address.
+
 buff=`curl -X POST http://127.0.0.1:18083/json_rpc -d '{"jsonrpc":"2.0","id":"0","method":"verify","params":{"data":"'"$challenge"'","address":"'"$address"'","signature":"'"$signature"'"}}' -H 'Content-Type: application/json'`
 
 pass="true"
 if echo "$buff" | grep -q "$pass"; then
  ph=${address:0:64};
  echo "$ph";
- sudo mount -t ecryptfs /home/user/lock-box /home/user/lock-box -o key=passphrase:passwd=$ph,no_sig_cache,ecryptfs_cipher=aes,ecryptfs_key_bytes=16,ecryptfs_passthrough=n,ecryptfs_enable_filename_crypto=n;
+ sudo mount -t ecryptfs $lock_box $lock_box -o key=passphrase:passwd=$ph,no_sig_cache,ecryptfs_cipher=aes,ecryptfs_key_bytes=32,ecryptfs_passthrough=n,ecryptfs_enable_filename_crypto=n;
 
- #gpg --passphrase "$address" -c /home/user/test.txt ;
- #srm /home/user/test.txt
  echo "Lock Box has been opened"
 else
- #gpg --passphrase "$address" --output /home/user/test.txt /home/user/test.txt.gpg;
 
- echo "Lock Box is now closed!";
+ echo "Authentication has failed!";
 fi
+
