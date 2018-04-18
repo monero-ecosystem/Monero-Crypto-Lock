73085374e8c0d4f87c5b2dbb44652a50f2c8734d
Douglas Bebber <dab@i2pmail.org>
2018-04-18 10:50:38 -0500

    Add files via upload
    Added lock-box as a command line argument and cleaned up some code.

--- demos/lock-box/lock-lock-box.sh
+++ demos/lock-box/lock-lock-box.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
+# 18 April 2018
+# Lock Monero Crypto-Lock lock-box
+# The lock-box to lock is passed as the only command line argument.
+# Must specify the full path for the lock-box
+
+if [ -z "$1" ]
+   then
+        echo "must specify full path specifier for lock-box!"
+        exit 1
+fi
+lock_box=$1
 
 function randomString {
         # if a param was passed, it's the length of the string we want
@@ -17,10 +28,9 @@ function randomString {
 
 randomString 10;
 echo "Challenge: $myRandomResult";
-# Collect challenge string, address, and signature from the console. Each parameter separated from each other by a space.
+
 read challenge address signature
 
-#echo "$challenge $address $signature"
 
 
 echo "################################"
@@ -31,22 +41,16 @@ echo "Signature: $signature"
 echo "################################"
 
 
-#curl -i -X POST -H "Content-Type: application/json" -d '{"jsonrpc":"2.0","id":"0","method":"getaddress"}' http://127.0.0.1:18082/json_rpc
-# Verify digital signature of challenge by address.
+
 buff=`curl -X POST http://127.0.0.1:18083/json_rpc -d '{"jsonrpc":"2.0","id":"0","method":"verify","params":{"data":"'"$challenge"'","address":"'"$address"'","signature":"'"$signature"'"}}' -H 'Content-Type: application/json'`
 
 pass="true"
 if echo "$buff" | grep -q "$pass"; then
- ph=${address:0:64};
- echo "$ph";
-# sudo mount -t ecryptfs /home/user/lock-box /home/user/lock-box -o key=passphrase:passwd=$ph,no_sig_cache,ecryptfs_cipher=aes,ecryptfs_key_bytes=16,ecryptfs_passthrough=n,ecryptfs_enable_filename_crypto=n;
- sudo umount /home/user/lock-box
+ sudo umount $lock_box
 
- #gpg --passphrase "$address" -c /home/user/test.txt ;
- #srm /home/user/test.txt
  echo "Lock Box has been closed"
 else
- #gpg --passphrase "$address" --output /home/user/test.txt /home/user/test.txt.gpg;
 
- echo "Lock Box is now open!";
+ echo "authentication failed!";
 fi
+
