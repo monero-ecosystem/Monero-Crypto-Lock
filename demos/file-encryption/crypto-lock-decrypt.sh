38a504f555757a1e1a15344ca1d0ce922dc87d2f
Douglas Bebber <dab@i2pmail.org>
2018-05-10 00:14:19 -0500

    Update crypto-lock-decrypt.sh

--- demos/file-encryption/crypto-lock-decrypt.sh
+++ demos/file-encryption/crypto-lock-decrypt.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
-
+# 18 April 2018.
+# This script decrypts a file that has been encrypted with
+# crypto-lock-encrypt.sh 
+#
+#The encrypted source file is removed after decryption.
+#
 # generate a random string for the challenge.
 function randomString {
         # if a param was passed, it's the length of the string we want
@@ -18,17 +23,29 @@ function randomString {
 
 randomString 10;
 echo "Challenge: $myRandomResult";
-read filename challenge address signature
+# Get the file to encrypt (full path), challenge string, address, and signature 
+from the console. each separated by a space.
+read target_file challenge address signature
 
-buff=`curl -X POST http://127.0.0.1:18083/json_rpc -d '{"jsonrpc":"2.0","id":"0","method":"verify","params":{"data":"'"$challenge"'","address":"'"$address"'","signature":"'"$signature"'"}}' -H 'Content-Type: application/json'`
+echo "################################"
+echo "Parameters provided:"
+echo "File to decrypt (full path): $target_file"
+echo "Challenge: $challenge"
+echo "Address: $address"
+echo "Signature: $signature"
+echo "################################"
+
+
+
+buff=`curl -X POST http://127.0.0.1:18083/json_rpc -d '{"jsonrpc":"2.0","id":"0"
+,"method":"verify","params":{"data":"'"$challenge"'","address":"'"$address"'","s
+ignature":"'"$signature"'"}}' -H 'Content-Type: application/json'`
 
 pass="true"
 if echo "$buff" | grep -q "$pass"; then
- fname=$(basename -- "$filename")
- extension="$fname##*.}"
- fname="${fname%.*}"
- path=$(dirname "${filename}")
- gpg --passphrase "$address" --output "$path"/"$fname"  "$filename" ;
+ gpg --passphrase "$address" --output /home/user/test.txt  $target_file ;
+ srm /home/user/test.txt.gpg # remove the encrypted source file.
 else
-   echo "Request is NOT authenticated!";
+
+ echo "request is NOT authorized!";
 fi
