ea5e148fcf98d60e95b2ed5f6ca8c7729342263c
Douglas Bebber <dab@i2pmail.org>
2018-09-19 15:29:07 -0500

    Update crypto-lock-encrypt.sh
    Removed using the Monero wallet public address as the gpg passphrase. Now using the private spend key for the gpg passphrase.

--- demos/file-encryption/crypto-lock-encrypt.sh
+++ demos/file-encryption/crypto-lock-encrypt.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
-# 18 April 2018
+# 19 September 2018
 # This script uses Monero Crypto-Lock to encrypt a specified file using gpg.
 # The source plaintext file is removed with secure delete.
-#
+# 
+# This version replaces the prior version which used the Monero wallet public address as
+# the gpg passphrase. This version uses the private spend key of the Monero wallet
+# as the gpg passphrase for file encryption.
 #
 # generate a random string for the challenge.
 function randomString {
@@ -35,18 +38,23 @@ echo "Signature: $signature"
 echo "################################"
 
 
-#curl -i -X POST -H "Content-Type: application/json" -d '{"jsonrpc":"2.0","id":"0","method":"getaddress"}' http://127.0.0.1:18082/json_rpc
-
 buff=`curl -X POST http://127.0.0.1:18083/json_rpc -d '{"jsonrpc":"2.0","id":"0","method":"verify","params":{"data":"'"$challenge"'","address":"'"$address"'","signature":"'"$signature"'"}}' -H 'Content-Type: application/json'`
 
 pass="true"
 if echo "$buff" | grep -q "$pass"; then
-# encrypt the file using the Monero public address as the passphrase.
- gpg --passphrase "$address" -c $target_file ; # Encrypt the file.
+# sign the address and use the private spend key as the gpg passphrase.
+sbuff=`curl -X POST http://127.0.0.1:18083/json_rpc -d '{"jsonrpc":".0","id":"0","method":"query_key","params":{"key_type":"spend_key"}}' -H 'Content-Type: application/json'`
+# parse the returned json.
+var="key"
+rt=$(echo "$sbuff" | grep "$var")
+var=${rt: -66}
+sk=${var:0:64}
+
+# encrypt the file using the private spend key as the passphrase.
+ gpg --passphrase "$sk" -c $target_file ; # Encrypt the file.
  srm $target_file # securely remove the source plaintext file.
 else
 
  echo "Authorization has failed!";
 fi
-
 
